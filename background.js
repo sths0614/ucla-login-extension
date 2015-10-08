1c921aefa78a1f9dc3aeaaba1c5398534d181b80
Stanley Hsu <sths0614@ucla.edu>
2015-10-08 03:20:44 -0700

    update to new routes

--- background.js
+++ background.js
@@ -2,18 +2,25 @@
  * Watchdog to check for a UCLA login page.
  */
 function checkForValidUrl(tabId, changeInfo, tab) {
-  // Page type 1
-  if(tab.url.indexOf('https://auth.ucla.edu/index.php') > -1) {
-  	chrome.tabs.executeScript(
-      null, {code:"if(document.getElementsByTagName('input').item(0).value==''){document.getElementsByTagName('input').item(0).value='"+localStorage["user"]+"';document.getElementsByTagName('input').item(1).value='"+sjcl.decrypt("javascript",localStorage["pass"])+"';document.forms.item(0).submit();}"});
-    chrome.pageAction.show(tabId);
-  }
-  // Page type 2
-  if (tab.url.indexOf('https://netaccess.logon.ucla.edu/cgi-bin/login') > -1
-    || tab.url.indexOf('https://wlanc.resnet.ucla.edu/cgi-bin/login') > -1) {
+  // // Page type 1
+  // if(tab.url.indexOf('https://auth.ucla.edu/index.php') > -1) {
+  // 	chrome.tabs.executeScript(
+  //     null, {code:"if(document.getElementsByTagName('input').item(0).value==''){document.getElementsByTagName('input').item(0).value='"+localStorage["user"]+"';document.getElementsByTagName('input').item(1).value='"+sjcl.decrypt("javascript",localStorage["pass"])+"';document.forms.item(0).submit();}"});
+  //   chrome.pageAction.show(tabId);
+  // }
+  // // Page type 2
+  // if (tab.url.indexOf('https://netaccess.logon.ucla.edu/cgi-bin/login') > -1
+  //   || tab.url.indexOf('https://wlanc.resnet.ucla.edu/cgi-bin/login') > -1) {
 
-  	chrome.tabs.executeScript(
-      null, {code:"if(document.getElementsByTagName('input')!=null){document.getElementsByTagName('input').item(1).value='"+localStorage["user"]+"';document.getElementsByTagName('input').item(2).value='"+sjcl.decrypt("javascript",localStorage["pass"])+"';document.forms.item(0).submit();}else{document.getElementsByTagName('a').item(0).click();}"});
+  // 	chrome.tabs.executeScript(
+  //     null, {code:"if(document.getElementsByTagName('input')!=null){document.getElementsByTagName('input').item(1).value='"+localStorage["user"]+"';document.getElementsByTagName('input').item(2).value='"+sjcl.decrypt("javascript",localStorage["pass"])+"';document.forms.item(0).submit();}else{document.getElementsByTagName('a').item(0).click();}"});
+  //   chrome.pageAction.show(tabId);
+  // }
+  // Updated
+  if(tab.url.indexOf('https://shb.ais.ucla.edu/shibboleth-idp/ucla/authn?') > -1) {
+    console.log('in if of background.js');
+    chrome.tabs.executeScript(
+      null, {code:"if(document.getElementById('logon').value==''){document.getElementById('logon').value='"+localStorage["user"]+"';document.getElementById('pass').value='"+sjcl.decrypt("javascript",localStorage["pass"])+"';document.forms.item(0).submit();}"});
     chrome.pageAction.show(tabId);
   }
 };
